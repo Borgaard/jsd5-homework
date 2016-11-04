9d560b284865e6afbcef4c96e9db7dca85a9bb93
matt <mtrower42@yahoo.com>
2016-11-04 08:52:34 -0700

    hw 1 and 2

---
+++ matt-Dragoris/hw_2/99bottles.js
@@ -0,0 +1,6 @@
+function sing() {
+	for (var i = 99; i > 0; i--) {
+		console.log( i, 'bottles of beer on the wall,',i,'bottles of beer\n', 'You take on down and pass it around ', i-1, 'bottles of ber on the wall!\n');
+	};
+	if (i===1) console.log(i, 'bottle of beer on the wall,',i,'bottles of beer\n', 'You take on down and pass it around ', i-1, 'bottles of ber on the wall!\n');
+}
