39a50c89f75dfd858dcfd4f31ba919f297435468
Andres Jimenez <andresjv@yahoo.com>
2016-11-07 18:10:26 -0800

    final version of the home work

--- andres-andresjv/homework_2/99-bottles-of-beer/app.js
+++ andres-andresjv/homework_2/99-bottles-of-beer/app.js
@@ -4,9 +4,50 @@
   
   Be sure that all of your output includes proper spacing (no words or values running into each other).
 
+99 bottles of beer on the wall, 99 bottles of beer.
+Take one down and pass it around, 98 bottles of beer on the wall.
+
+
+
+
   Hint: You can make your output appear on multiple lines by using the string \n to insert a line break.
   
   For an extra challenge, improve this code to print "1 bottle" rather than "1 bottles." (Hint: read up on if/else
   conditional statements at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else). 
 */
 
+
+
+// Initiale Variable Bottles and assign value
+var bottlesTotal = 99;
+// Create variables for the different parts of the sentence 
+var firstLineI = " bottles of beer on the wall, "
+var firstLineII = " bottles of beer. \n";
+var secondLinePartI = "Take one down and pass it around, ";
+var secondLinePartII = " bottles of beer on the wall.";
+
+//Count down loop
+while(bottlesTotal > 0){
+
+  //Check if bottles are grater than 1
+  if(bottlesTotal > 1){
+      console.log(bottlesTotal + firstLineI + bottlesTotal + firstLineII + secondLinePartI + (bottlesTotal-1) +secondLinePartII);
+  } else {
+        // if bottles are === 1
+        // Create a new variable that replaces the word bottles to bottle
+      var firstLineI_1 = firstLineI.replace("bottles", "bottle");
+      var firstLineII_1 = firstLineII.replace("bottles", "bottle");
+      console.log(bottlesTotal + firstLineI_1 + bottlesTotal + firstLineII_1 + secondLinePartI + (bottlesTotal-1) +secondLinePartII);
+  };
+
+  bottlesTotal --;
+}
+
+console.log("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.");
+bottlesTotal = 99;
+
+/*
+
+99 bottles of beer on the wall, 99 bottles of beer.
+Take one down and pass it around, 98 bottles of beer on the wall.
+*/
