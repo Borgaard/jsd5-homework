317cf5d2d19c44a28a4c945af0662478a9173160
matt <mtrower42@yahoo.com>
2016-11-10 21:29:50 -0800

    random address generator

--- matt-Dragoris/hw_3/address-generator/app.js
+++ matt-Dragoris/hw_3/address-generator/app.js
@@ -8,3 +8,14 @@ Optional bonus work on conditionals and functions:
     
     34578 Dolphin Street, Wonka NY, 44506
 */
+(function randomAddress() {
+	var streetName = ['Warren', 'Bank', 'Valley', 'View', 'Highland', 'Route', 'Buttonwood', 'York', 'Willow', 'Liberty', 'Front', 'Maple', 'Riverside', 'Locust', 'Route', 'College', 'John', 'Willow', 'Adams','Laurel', 'Grant', 'Garfield', 'Franklin'];
+	var streetType = ['Street', 'Road', 'Way', 'Court', 'Drive'];
+	var streetNumber = Math.round(Math.random() * 10000);
+	function randNum(min, max){
+		return Math.round(Math.random()* (max - min) + min)
+	}
+	var randomStreet = streetName[randNum(0,streetName.length-1)];
+	var randomType = streetType[randNum(0,streetType.length-1)]
+	return streetNumber + ' ' + randomStreet + ' ' + randomType
+})();
