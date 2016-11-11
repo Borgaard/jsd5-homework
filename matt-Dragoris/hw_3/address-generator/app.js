a821651e8fc6e96278121a7b8b1061fc8d7cc0ef
matt <mtrower42@yahoo.com>
2016-11-11 10:01:59 -0800

    will now return an array of addresses, if given a number as a parameter. Added state and zip, reorginized code into functions.

--- matt-Dragoris/hw_3/address-generator/app.js
+++ matt-Dragoris/hw_3/address-generator/app.js
@@ -8,14 +8,41 @@ Optional bonus work on conditionals and functions:
     
     34578 Dolphin Street, Wonka NY, 44506
 */
-(function randomAddress() {
-	var streetName = ['Warren', 'Bank', 'Valley', 'View', 'Highland', 'Route', 'Buttonwood', 'York', 'Willow', 'Liberty', 'Front', 'Maple', 'Riverside', 'Locust', 'Route', 'College', 'John', 'Willow', 'Adams','Laurel', 'Grant', 'Garfield', 'Franklin'];
+
+(function getAddress(num) {
+	var addressArray = [];
+	var streetName = ['Warren', 'Bank', 'Valley', 'View', 'Highland', 'Route', 'Buttonwood', 'York', 'Liberty', 'Front', 'Maple',
+					'Riverside', 'Locust', 'Route', 'College', 'John', 'Willow', 'Adams', 'Grant', 'Garfield', 'Franklin', 'Laurel',
+					'West', 'Carriage', 'Market', 'Deerfield ', 'River', 'Cemetery' ,'Clinton', 'Surrey', 'Sherwood', 'Delaware', 
+					'Madison', 'Grove', 'George', 'Ivy','9th', 'Homestead'];
 	var streetType = ['Street', 'Road', 'Way', 'Court', 'Drive'];
-	var streetNumber = Math.round(Math.random() * 10000);
-	function randNum(min, max){
-		return Math.round(Math.random()* (max - min) + min)
+	var states = ['AL','AK','AZ','AR','CA','CO','CT','DE','DC','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MT',
+				'NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','MD','MA','MI','MN','MS','MO','PA','RI','SC','SD','TN','TX',
+				'UT','VT','VA','WA','WV','WI','WY'];
+	
+	function createRandomAddress() {
+		
+		function randNum(min, max){
+			return Math.round(Math.random()* (max - min) + min);
+		}
+
+		var streetNumber = Math.round(Math.random() * 10000);
+		var zip = Math.round(Math.random()* (99999 - 10000) + 10000);
+		var randomStreet = streetName[randNum(0,streetName.length-1)];
+		var randomType = streetType[randNum(0,streetType.length-1)];
+		var randomState = states[randNum(0,states.length-1)];
+		var address = streetNumber + ' ' + randomStreet + ' ' + randomType + ', ' + randomState + ', ' + zip
+			
+		addressArray.push(address);
+
+		return address
 	}
-	var randomStreet = streetName[randNum(0,streetName.length-1)];
-	var randomType = streetType[randNum(0,streetType.length-1)]
-	return streetNumber + ' ' + randomStreet + ' ' + randomType
+	
+	if (num) {
+		for (var i = 0; i < num; i++) { 
+			createRandomAddress(); 
+		}
+	} else createRandomAddress();
+
+	return addressArray
 })();
