4c3fd22fa48501af6f3035fa6091651284110412
Andres Jimenez <andresjv@yahoo.com>
2016-11-12 09:56:25 -0800

    updated else if statememts

--- andres-andresjv/homework_3/1-ages-lab/app.js
+++ andres-andresjv/homework_3/1-ages-lab/app.js
@@ -17,22 +17,24 @@
 */
 
 
-var age = 64;
-
-if(age < 16){
-    console.log('you cannot do much outside of going to school');
-
-}else if(age > 16 && age < 18){
-    console.log('you can drive');
+// Function that displays privilege you receive based on your agePrivilege
+function agePrivilege(age){
     
-}else if(age >= 18 && age < 21){
-    console.log('you can vote');
-}else if(age >= 21 && age < 25){
-    console.log('you can drink alcohol');
-}else if(age >= 25 && age < 35){
-    console.log('you can rent a car');
-}else if(age >= 35 && age < 62){
-    console.log('you can run for president');
-}else if(age >= 62){
-    console.log('you collect social security benefits');
+    if(age >= 62){
+        console.log('you collect social security benefits');
+    }else if(age >= 35) {
+        console.log('you can run for president');
+    }else if (age >= 25){
+        console.log('you can rent a car');
+    }else if (age >= 21){
+        console.log('you can drink alcohol');
+    }else if(age >= 18){
+        console.log('you can vote');
+    }else if (age >= 16){
+        console.log('you can drive');
+    }
+
 }
+
+
+
